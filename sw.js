be57d552e96f8b9ebc224e723b810eae64844466
Isamu YANO <isamuyano@gmail.com>
2026-06-19 14:53:31 +0900

    Remove push notifications and add install prompt

--- sw.js
+++ sw.js
@@ -5,24 +5,3 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  const targetUrl = event.notification.data?.url || self.registration.scope;
-
-  event.waitUntil(
-    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clients) => {
-      const target = new URL(targetUrl, self.registration.scope);
-      const existingClient = clients.find((client) => {
-        return new URL(client.url).pathname === target.pathname;
-      });
-
-      if (existingClient) {
-        return existingClient.focus();
-      }
-
-      return self.clients.openWindow(target.href);
-    })
-  );
-});
